a27fe36cfc65df4f34ae1202b1fded7f6e35357e
marinafaggian <marinafaggian92@gmail.com>
2023-08-07 11:33:20 -0300

    soluciono la construccion de alumno

--- js/proyecto.js
+++ js/proyecto.js
@@ -1,45 +1,53 @@
+let form = document.getElementById("form");
 let nombre = document.getElementById("nombre");
 let apellido = document.getElementById("apellido");
 let btn = document.getElementById("btn");
 let alumno;
-let notas = Array.from(document.getElementsByClassName("input nota"));
+let notas = document.getElementsByClassName("input nota");
+let arrayNotas = [];
+    
 
 class Alumno {
-    constructor(nombre, apellido) {
+    constructor(nombre, apellido, notas) {
         this.nombre = nombre;
         this.apellido = apellido;
-        this.notas = notas;
+        this.notas = arrayNotas;
     }
 }
 
+// form.addEventListener("submit", function(e))
+//     e.preventDefault();
 
 function crearAlumno(e) {
     console.log("Hola, funcioná");
     e.preventDefault();
     let nombre1 = nombre.value;
     let apellido1 = apellido.value;
-    let notas1 = notas.value;
     nombre.value = "";
     apellido.value = "";
+    let notas1 = arrayNotas.value;
+    for (i = 0; i < notas.length; i++) {
+        arrayNotas.push(notas[i].value);
+        notas [i].value = "";
+    }
     alumno = new Alumno(nombre1, apellido1, notas1);
     console.log(alumno);
 }
 
-let obtenerNotasAprobadas = (alumno) => {
-    let notasAprobadas = alumno.notas.filter((nota) => nota >= 7);
-    console.log(notasAprobadas, "Estas son las notas aprobadas");
-    return notasAprobadas;
-};
+// let obtenerNotasAprobadas = (alumno) => {
+//     let notasAprobadas = alumno.notas.filter((nota) => nota >= 7);
+//     console.log(notasAprobadas, "Estas son las notas aprobadas");
+//     return notasAprobadas;
+// };
 function sumarNotas(alumno) {
     const suma = alumno.notas.reduce((acumulador, nota) => acumulador + nota, 0);
     console.log(suma,"Esto es la suma del array");
     return suma;
 }
 
-// let notas = cargarNotas();
 // let aprobadas = obtenerNotasAprobadas(alumno);
-// let sumaDeNotas = sumarNotas(alumno);
-// let promedio = sumaDeNotas / alumno.notas.length;
+let sumaDeNotas = sumarNotas(alumno);
+let promedio = sumaDeNotas / alumno.notas.length;
 
 // if (promedio >= 7) {
 //     console.log(alumno.nombre + ", tu promedio es de " + Math.round(promedio) + ". Tu materia está aprobada");
@@ -53,3 +61,4 @@ function sumarNotas(alumno) {
 
 
 btn.addEventListener("click", crearAlumno);
+console.log(promedio, "este es el promedio de las notas");
