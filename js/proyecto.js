7c965e5faee37ae448a373aec1b68d6888f705c8
marinafaggian <marinafaggian92@gmail.com>
2023-08-09 16:55:50 -0300

    funcionan las funciones de sumar notas y promedio

--- js/proyecto.js
+++ js/proyecto.js
@@ -2,23 +2,31 @@ let form = document.getElementById("form");
 let nombre = document.getElementById("nombre");
 let apellido = document.getElementById("apellido");
 let btn = document.getElementById("btn");
-let alumno;
 let notas = document.getElementsByClassName("input nota");
-let arrayNotas = [];
-    
+let alumno;
+let notas1 = notas.value;
+let sumaDeNotas;
+let prom;
+
 
 class Alumno {
-    constructor(nombre, apellido, notas) {
+    constructor(nombre, apellido,) {
         this.nombre = nombre;
         this.apellido = apellido;
-        this.notas = arrayNotas;
+        this.arrayNotas = [];
     }
 }
 
 function sumarNotas(alumno) {
-    const suma = alumno.notas.reduce((acumulador, nota) => acumulador + nota, 0);
+    const suma = alumno.arrayNotas.reduce((acumulador, nota) => acumulador + parseInt(nota), 0);
     console.log(suma,"Esto es la suma del array");
     return suma;
+}
+
+function promedio(alumno) {
+    const promedio1 = sumaDeNotas/alumno.arrayNotas.length;
+    console.log(promedio1, "promedio")
+    return promedio1;
 }
 
 function crearAlumno(e) {
@@ -26,37 +34,16 @@ function crearAlumno(e) {
     e.preventDefault();
     let nombre1 = nombre.value;
     let apellido1 = apellido.value;
-    nombre.value = "";
-    apellido.value = "";
-    let notas1 = arrayNotas.value;
+    alumno = new Alumno(nombre1, apellido1, notas1);
     for (i = 0; i < notas.length; i++) {
-        arrayNotas.push(notas[i].value);
+        alumno.arrayNotas.push(notas[i].value);
         notas [i].value = "";
     }
-    alumno = new Alumno(nombre1, apellido1, notas1);
-    let sumaDeNotas = sumarNotas(alumno);
     console.log(alumno);
-    console.log(sumaDeNotas);
+    sumaDeNotas = sumarNotas(alumno);
+    prom = promedio(alumno);
+    nombre.value = "";
+    apellido.value = "";
 }
-
-// let obtenerNotasAprobadas = (alumno) => {
-//     let notasAprobadas = alumno.notas.filter((nota) => nota >= 7);
-//     console.log(notasAprobadas, "Estas son las notas aprobadas");
-//     return notasAprobadas;
-// };
-
-// let aprobadas = obtenerNotasAprobadas(alumno);
-// let promedio = sumaDeNotas / alumno.notas.length;
-// if (promedio >= 7) {
-//     console.log(alumno.nombre + ", tu promedio es de " + Math.round(promedio) + ". Tu materia está aprobada");
-// } else {
-//     console.log(alumno.nombre + ", tu promedio es de " + Math.round(promedio) + ". Tu materia está desaprobada");
-// }
-
-// promedio >= 7 ? console.log(alumno.nombre + ", tu promedio es de " + Math.round(promedio) + ". Tu materia está aprobada") : console.log(alumno.nombre + ", tu promedio es de " + Math.round(promedio) + ". Tu materia está desaprobada")
-
-// console.log(promedio, "Este es el promedio sin redondear");
-
-
+          
 btn.addEventListener("click", crearAlumno);
-// console.log(promedio, "este es el promedio de las notas");
